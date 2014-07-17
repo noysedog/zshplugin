61dbdcd4e1f63d6cfbe93c6f8ca8114e16790468
noyse_dog <noysedog@users.noreply.github.com>
2014-07-17 20:58:32 +0200

    Update noyse.plugin.zsh

--- noyse.plugin.zsh
+++ noyse.plugin.zsh
@@ -47,6 +47,31 @@ function ripigliami()
 	echo "Non ti sei ancora ripigliato?"
 }
 
+# Estrazione archivi
+extract () {
+     if [ -f $1 ] ; then
+         case $1 in
+             *.tar.bz2)   tar xjf $1        ;;
+             *.tar.gz)    tar xzf $1     ;;
+             *.bz2)       bunzip2 $1       ;;
+             *.rar)       rar x $1     ;;
+             *.gz)        gunzip $1     ;;
+             *.tar)       tar xf $1        ;;
+             *.tbz2)      tar xjf $1      ;;
+             *.tgz)       tar xzf $1       ;;
+             *.zip)       unzip $1     ;;
+             *.Z)         uncompress $1  ;;
+             *.7z)        7z x $1    ;;
+             *)           echo "'$1' non può essere estratto con extract()" ;;
+         esac
+     else
+         echo "'$1' file non valido"
+     fi
+}
+
+
+
+
 #function rawr() {
 #	cd ~/.mybin/rawr
 #	sudo python ./rawr.py 
