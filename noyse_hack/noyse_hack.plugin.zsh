0342b22fa05873819892b90e54b02839c764b461
noyse_dog <noysedog@users.noreply.github.com>
2019-12-05 18:57:34 +0100

    nuova versione

--- noyse_hack/noyse_hack.plugin.zsh
+++ noyse_hack/noyse_hack.plugin.zsh
@@ -1,12 +1,73 @@
-#SEC TOOL#
-alias strip='sudo ~/.mybin/./autostrip.sh'
-alias karma='sudo ~/.mybin/./karmaplay.sh'
-alias metpay='sudo ~/.mybin/./MetPay.sh'
-alias gerix='sudo python ~/.mybin/gerix-wifi-cracker-master/./gerix.py'
-alias rawr="sudo python ~/.mybin/rawr/./rawr.py"
-alias stf="sudo ~/.mybin/HconSTF/./HconSTF"
-#-------------------------------------#
+#versione:      2.6.16.16
+#data.Agg:      2/12/19
+
+#######################################SEC TOOL# 
+
+#--SCRIPT --------------------------# 
+alias strip='~/.mybin/./autostrip.sh'
+alias karma='~/.mybin/./karmaplay.sh'
+alias metpay='~/.mybin/./MetPay.sh'
+alias setmon='sudo ~/.mybin/./setmon.sh'
+
+#--GIT-----------------------------------------#
+alias Listgit="ls -alh /home/noyse/Modelli/Project/GIT"
+
+#----------------------------------  1_OSINT --#
+alias OSINT='cd /home/noyse/Modelli/Project/GIT/1_Osint_InfoG/'
+alias CM='sudo /home/noyse/Modelli/Project/GIT/1_Osint_InfoG/cmsmap/./cmsmap.py'
+alias datasploit='sudo /home/noyse/Modelli/Project/GIT/1_Osint_InfoG/datasploit/./domainOsint.py' 
+alias eye='python3 /home/noyse/Modelli/Project/GIT/1_Osint_InfoG/EyeWitness/EyeWitness.py'
+
+#------------------------------------ 2_Nmap --#
+alias MAP='cd /home/noyse/Modelli/Project/GIT/2_Nmap/'
+alias autonse='sudo /home/noyse/Modelli/Project/GIT/2_Nmap/AutoNSE/./autonse.sh'
+
+#------------------------------------ 3_List --#
+alias LIST='cd /home/noyse/Modelli/Project/GIT/3_List/'
+alias wlist='wordlists'
+alias fuzzdb='ls -hl /home/noyse/Modelli/Project/GIT/3_List/fuzzdb/'
+
+#--------------------------------- 4_Exploit --#
+alias EXP='cd  /home/noyse/Modelli/Project/GIT/4_Exploit/'
+alias evading='sudo /home/noyse/Modelli/Project/GIT/4_Exploit/macro_pack/src/./macro_pack.py'
+alias routexploit='/home/noyse/Modelli/Project/GIT/4_Exploit/routersploit/.rsf.py'
+alias evilgrade='sudo /home/noyse/Modelli/Project/GIT/4_Exploit/evilgrade/./evilgrade'
+
+#---------------------------------- 5_WebApp --#
+alias WEB='cd /home/noyse/Modelli/Project/GIT/5_WebApp/'
+alias dotdotpwn='cd /home/noyse/Modelli/Project/GIT/5_WebApp/dotdotpwn/ && ./dotdotpwn.pl'
+alias panoptic='sudo /home/noyse/Modelli/Project/GIT/5_WebApp/Panoptic/./panoptic.py'
+alias wascan='sudo /home/noyse/Modelli/Project/GIT/5_WebApp/wascan/./wascan.py'
+alias Back='weevely generate AllaCarica ~/DirtyWorks/weevely/`ora`.txt'
+
+#------------------------------------ 6_WiFi --#
+alias WiFi='cd /home/noyse/Modelli/Project/GIT/6_WiFi'
+alias chekill='sudo airmon-ng check kill'
+alias dumplive='sudo airodump-ng wlo1mon'
+alias mon0='sudo airmon-ng start wlo1'
+
+#------------------------------ 7_NetWorking --#
+alias NET='cd /home/noyse/Modelli/Project/GIT/7_Networking'
+alias ridelay='python /home/noyse/Modelli/Project/GIT/7_Networking/ridrelay/ridrelay.py'
+
+#----------------------------- 8_Weaponizing --#
+alias WPN='cd /home/noyse/Modelli/Project/GIT/8_Weaponization/'
+
+
+
+#--GIT-END-------------------------------------#
+
+
+#--METASPLOIT---------------------------------#
 alias msf='sudo msfconsole'
-alias sniff='sudo ettercap -G &'
-alias shark='sudo wireshark &'
-##################EOF##################
+alias msf_doc='/usr/share/metasploit-framework/documentation'
+alias msf_data='cd /usr/share/metasploit-framework/data'
+alias venom='sudo msfvenom'
+
+#--NETWORK-------------------------------------#
+alias sniff='ettercap -G &'
+alias shark='wireshark &'
+
+
+######################EOF#######################
+
