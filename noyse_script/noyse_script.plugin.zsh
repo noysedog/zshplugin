2ea6f4126d7d7d22577ce3b6fc07531e120e931e
noyse_dog <noysedog@users.noreply.github.com>
2019-12-05 18:58:54 +0100

    nuova versione

--- noyse_script/noyse_script.plugin.zsh
+++ noyse_script/noyse_script.plugin.zsh
@@ -1,18 +1,33 @@
-##Terminal shortcut
-alias -g N='> /dev/null'
-alias -g G='| grep'
-alias -g C='| ccze -A'
-alias -g line='| wc -l'
-alias -g Sn='| sort -n'
-alias -g Snr='| sort -nr'
-alias -g H=' --help'
+#versione:      2.0.3.1
+#data.Agg:      28/10/19
 
-##Ruby
-alias pry= "pry --simple-prompt"
+## FUNZIONI ##
 
-##Perl
-#alias CPAN
+## Cazzata... Pensaci un attimo...
+function aspe() {
+	#Pensaci un attimo#
+	echo "Pensaci un attimo:"
+	echo "Hai 10 secondi per decidere..."
+	for i in {0..9}; do echo -ne "."; sleep 1; done
+}
+
+
+## Altra cazzata, ma utili info!
+function ripigliami() {
+Distro=`cat /etc/lsb-release  |grep DISTRIB_ID= | cut -f 2 -d "="`
+
+	#Quando sei perso, e non sai che fare
+	#questa funzione fa a caso tuo!!
+	echo "Sei qui: $PWD"
+	echo "Sei tu: `whoami`"
+	echo "$Distro"
+	echo "Con questo UID: $UID"
+	echo "Alle: `date '+%X' `"
+	echo "Up: `uptime`"
+	echo "Non ti sei ancora ripigliato?"
+}
+
 
 #-------------------------------------#
 
-##################EOF##################
+###################EOF#################
